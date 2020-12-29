5a971a233848eafa56fd57e8f989c3eda00453ce
Kalib <kalibuas@gmail.com>
2020-12-30 06:02:24 +0800

    initialize

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
 
 function App() {
   return (
     <>
-      <h1> Welcome to Proshop </h1>
+    <main>
+      <Header/>
+      <h1>Welcome to Proshop</h1>
+      <Footer/> 
+      </main>
     </>
   );
 }
